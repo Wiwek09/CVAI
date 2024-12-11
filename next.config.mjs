cc5ebd8daec19d557c6cbd96b96ed1ede5fa75a3
Wiwek09 <kcbibekmail@gmail.com>
2024-12-11 11:46:09 +0545

    refactor: chaning production to local

--- next.config.mjs
+++ next.config.mjs
@@ -6,25 +6,25 @@ const nextConfig = {
     return config;
   },
   // Production
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cvai.rebuzzpos.com",
-        pathname: "/cv_images/**",
-      },
-    ],
-  },
-  // Local
   // images: {
   //   remotePatterns: [
   //     {
   //       protocol: "https",
-  //       hostname: "cvai.azurewebsites.net",
+  //       hostname: "cvai.rebuzzpos.com",
   //       pathname: "/cv_images/**",
   //     },
   //   ],
   // },
+  // Local
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cvai.azurewebsites.net",
+        pathname: "/cv_images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
