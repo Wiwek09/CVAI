ad3e85580b9a5435bc64656261c91fe7d26f16e5
Bibek  K.C.  ( Wiwek K.C. ) <52911586+Wiwek09@users.noreply.github.com>
2025-02-05 14:55:40 +0545

    Update index.ts

--- app/dashboard/store/index.ts
+++ app/dashboard/store/index.ts
@@ -1,12 +1,26 @@
 import { create } from "zustand";
 
+//Folder Select
 interface FolderSelectStore {
   selectFolderId: string | null;
   setSelectFolderId: (id: string | null) => void;
 }
 
+// DropDown of Public Folder
+interface PublicFolderState {
+  isFolderListOpen: boolean;
+  toogleFolderList: () => void;
+}
+
+// Folder Select
 export const folderSelectStore = create<FolderSelectStore>((set) => ({
   selectFolderId: null,
   setSelectFolderId: (id) => set({ selectFolderId: id }),
 }));
 
+// DropDown of Public Folder
+export const publicFolderStore = create<PublicFolderState>((set) => ({
+  isFolderListOpen: true,
+  toogleFolderList: () =>
+    set((state) => ({ isFolderListOpen: !state.isFolderListOpen })),
+}));
